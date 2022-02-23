a539692c6d0c283997fe4e639f4e1c1075d2439b
abdul hamid <abdulhamidnugroho@gmail.com>
2022-02-23 23:00:38 +0700

    jwt

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -16,18 +16,18 @@ class UserTableSeeder extends Seeder
     {
         DB::table('users')->insert([
             [
-                'name'      => 1,
-                'username'  => 2,
+                'name'      => 'Abdul',
+                'username'  => 'abdul',
                 'password'  => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password,
             ],
             [
-                'name'      => 1,
-                'username'  => 3,
+                'name'      => 'Hamid',
+                'username'  => 'hamid',
                 'password'  => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password,
             ],
             [
-                'name'      => 2,
-                'username'  => 2,
+                'name'      => 'Bjorn Ironside',
+                'username'  => 'ironside',
                 'password'  => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password,
             ],
         ]);
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ReportController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -19,5 +20,16 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::get('/user-profile', [AuthController::class, 'userProfile']);    
+});
+
+Route::post('auth/login', [AuthController::class, 'login']);
 Route::get('merchants/report', [ReportController::class, 'getMerchantOmzetReport']);
 Route::get('outlets/report', [ReportController::class, 'getMerchantOutletOmzetReport']);
